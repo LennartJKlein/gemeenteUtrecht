1941f678d07096f4c84e26269a2170778b680ca3
LennartKlein <info@lennartklein.nl>
2015-12-16 02:03:30 +0100

    Subpagina's algemene structuur

--- directmessage-responsive.js
+++ directmessage-responsive.js
@@ -1,10 +1,13 @@
 $(document).ready(function(){
-	if ($(window).width() < 990) {
+	if ($(window).width() <= 990) {
 		$('.rslides img').attr('width','');
 		$('.rslides img').attr('height','');
 	}
 	
-	$('#mainMenu').prepend('<div class="menu-toggle">Menu<i class="fa fa-bars"></i></div>');
+	if ($(window).width() <= 768) {
+		$('#mainMenu').prepend('<div class="menu-toggle">Menu<i class="fa fa-bars"></i></div>');
+	}
+	
 	$(document).on('click','.menu-toggle', function(){
 		$('#mainMenu ul').slideToggle();
 	});
